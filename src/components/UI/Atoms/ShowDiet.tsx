7e7d943460e35dfaca7c4d491fa0b1c748d0fa41
Gabriel <gabriel.krapp@hotmail.com>
2024-01-02 22:40:43 -0300

    Creating the logic to place the diets

--- src/components/UI/Atoms/ShowDiet.tsx
+++ src/components/UI/Atoms/ShowDiet.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { ShowDietProps } from '@/interfaces/ShowDietProps';
+import React from 'react';
 
-export const ShowDiet = () => {
+export const ShowDiet: React.FC<ShowDietProps> = ({ diets }) => {
   return (
-    <div className="w-32 h-32 border-2 border-black rounded-lg flex flex-col items-center justify-center font-mono text-sm bg-white hover:border-[#18E1C2] hover:bg-[#18E1C2] hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-200 ml-10">
-      <p>Dieta 1</p>
+    <div>
+      {diets.map((diet, index) => (
+        <div key={index} className="w-32 h-32 border-2 border-black rounded-lg flex flex-col items-center justify-center font-mono text-sm bg-white hover:border-[#18E1C2] hover:bg-[#18E1C2] hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-200 ml-10">
+          <p>{diet.name}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { CreateDiet } from "@/components/UI/Atoms/CreateDiet";
 import { ShowDiet } from "@/components/UI/Atoms/ShowDiet";
 import { Navbar } from "@/components/UI/Organisms/Navbar";
 import { LoadingComponent } from '@/components/UI/Atoms/LoadingComponent';
-import { useCreateDiet } from '@/functions/create-diet';
+import { useCreateDiet } from '@/hooks/useCreateDiets';
+import { useFetchDiets } from '@/hooks/useFetchDiets';
+import { CustomAlert } from '@/components/UI/Atoms/Alerts';
 
 function Home() {
-  const { mutate, isLoading } = useCreateDiet();
+  const { mutate, isLoading, isError } = useCreateDiet();
+  const { data: diets, refetch } = useFetchDiets();
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const handleCreateDiet = () => {
+    mutate();
+  };
 
   if (isLoading) {
     return <LoadingComponent />;
@@ -15,9 +27,12 @@ function Home() {
   return (
     <main>
       <Navbar />
+      {isError && (
+        <CustomAlert severity="error">Ocorreu um erro ao carregar suas dietas.</CustomAlert>
+      )}
       <div className="w-full items-center justify-center font-mono text-sm lg:flex mt-5">
-        <CreateDiet createDietHandler={() => mutate()} />
-        <ShowDiet />
+        <CreateDiet createDietHandler={handleCreateDiet} />
+        {diets && diets.length > 0 && <ShowDiet diets={diets} />}
       </div>
     </main>
   );
