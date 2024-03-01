2811db565e623e6f2b5212cadd2547470de6e132
Gabriel <gabriel.krapp@hotmail.com>
2024-03-01 18:30:26 -0300

    crating middleware verifytoken

---
+++ BackEnd/src/middleware/verifyToken.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
