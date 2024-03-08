1e55c00c393513ac6f7c780647f369401296518c
Gabriel <gabriel.krapp@hotmail.com>
2024-03-07 21:10:24 -0300

    Creating route GetDiet

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -4,6 +4,7 @@ import register from './routers/Register/Register';
 import login from './routers/Login';
 import createDiet from './routers/CreateDiet';
 import getdiets from './routers/GetDiets';
+import getdiet from './routers/GetDiet';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(register);
 app.use(login);
 app.use(createDiet);
 app.use(getdiets);
+app.use(getdiet);
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
--- FrontEnd/src/hooks/useFetchDietDetails.ts
+++ FrontEnd/src/hooks/useFetchDietDetails.ts
@@ -3,7 +3,7 @@ import axiosInstance from '@/utils/axiosInstance';
 
 export const useFetchDietDetails = (dietId: any) => {
   return useQuery(['dietDetails', dietId], async () => {
-    const { data } = await axiosInstance.get(`/diets/${dietId}`);
+    const { data } = await axiosInstance.get(`/getDiet/${dietId}`);
     return data;
   }, {
     enabled: !!dietId,
